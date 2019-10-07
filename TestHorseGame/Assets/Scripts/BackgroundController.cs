122d558aa4f81046254fa3c9367a80411ddc6f42
Vavashka23 <vova.kupreev.23@mail.ru>
2019-10-07 22:29:21 +0300

    Add game field and 3 buttons for level change

--- TestHorseGame/Assets/Scripts/BackgroundController.cs
+++ TestHorseGame/Assets/Scripts/BackgroundController.cs
@@ -15,8 +15,7 @@ public class BackgroundController : MonoBehaviour
         _image = gameObject.GetComponent<Image>();
         _panel = gameObject.GetComponent<RectTransform>();
         screenSizeY = (Screen.height - 2048);
-        Debug.Log(Screen.height);
-        Debug.Log(screenSizeY*4.5f);
+        Debug.Log(Screen.width);
         if(Screen.height > 2048)
         {
             _panel.sizeDelta = new Vector2(_panel.sizeDelta.x, _panel.sizeDelta.y + (screenSizeY*4.5f));
