12b909f982a6b1d2a6a55f21afd51f1b2284f588
Vavashka23 <vova.kupreev.23@mail.ru>
2019-10-07 20:10:30 +0300

    Make adaptive background

---
+++ TestHorseGame/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundController : MonoBehaviour
+{
+    private Image _image;
+    private RectTransform _panel;
+    private Sprite destSprite;
+    private int screenSizeY;
+
+    void Start()
+    {
+        _image = gameObject.GetComponent<Image>();
+        _panel = gameObject.GetComponent<RectTransform>();
+        screenSizeY = (Screen.height - 2048);
+        Debug.Log(Screen.height);
+        Debug.Log(screenSizeY*4.5f);
+        if(Screen.height > 2048)
+        {
+            _panel.sizeDelta = new Vector2(_panel.sizeDelta.x, _panel.sizeDelta.y + (screenSizeY*4.5f));
+        }
+    }
+}
