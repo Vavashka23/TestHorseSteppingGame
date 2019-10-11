ded402538d4c9256d79e95406a1427d4d8f6337c
Vavashka23 <vova.kupreev.23@mail.ru>
2019-10-11 14:33:28 +0300

    Add moving coroutines. Fix scaling when levels changed

--- TestHorseGame/Assets/Scripts/LevelSwitcher.cs
+++ TestHorseGame/Assets/Scripts/LevelSwitcher.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class LevelSwitcher : MonoBehaviour
 {
-    public GameObject nine_field;
-    public GameObject seven_field;
-    public GameObject five_field;
+    public GameObject nine_field, seven_field, five_field;
+    public GameObject player;
+    private Vector4 posFive, posSeven, posNine;
+    private int level = 5;
 
 
     void Start()
@@ -15,6 +16,10 @@ public class LevelSwitcher : MonoBehaviour
         nine_field = GameObject.FindGameObjectWithTag("NineField");
         seven_field = GameObject.FindGameObjectWithTag("SevenField");
         five_field = GameObject.FindGameObjectWithTag("FiveField");
+        player = GameObject.FindGameObjectWithTag("Horse");
+        posFive = new Vector4(Screen.width/(5*2), Screen.height/2+Screen.width/2 - Screen.width/(2*5), 1f, 1f);
+        posSeven = new Vector4(Screen.width/(7*2), Screen.height/2+Screen.width/2 - Screen.width/(2*7), 1f, 1f);
+        posNine = new Vector4(Screen.width/(9*2), Screen.height/2+Screen.width/2 - Screen.width/(2*9), 1f, 1f);
         FiveClick();
     }
 
@@ -23,6 +28,9 @@ public class LevelSwitcher : MonoBehaviour
         nine_field.SetActive(true);
         seven_field.SetActive(false);
         five_field.SetActive(false);
+        level = 9;
+        player.transform.position = new Vector2(posNine.x, posNine.y);
+        player.transform.localScale = new Vector3(0.6f, 0.6f, 1f);
     }
 
     public void SevenClick()
@@ -30,6 +38,9 @@ public class LevelSwitcher : MonoBehaviour
         nine_field.SetActive(false);
         seven_field.SetActive(true);
         five_field.SetActive(false);
+        level = 7;
+        player.transform.position = new Vector2(posSeven.x, posSeven.y);
+        player.transform.localScale = new Vector3(0.7f, 0.7f, 1f);
     }
 
     public void FiveClick()
@@ -37,5 +48,8 @@ public class LevelSwitcher : MonoBehaviour
         nine_field.SetActive(false);
         seven_field.SetActive(false);
         five_field.SetActive(true);
+        level = 5;
+        player.transform.position = new Vector2(posFive.x, posFive.y);
+        player.transform.localScale = new Vector3(1f, 1f, 1f);
     }
 }
--- TestHorseGame/Assets/Scripts/PlayerMoving.cs
+++ TestHorseGame/Assets/Scripts/PlayerMoving.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMoving : MonoBehaviour
 {
-    public GameObject nine_field;
-    public GameObject seven_field;
-    public GameObject five_field;
+    public GameObject nine_field, seven_field, five_field;
     private int level = 5;
-    private bool isMoving = false;
-    private Vector2 currentPos, targetPos, fieldY;
+    private bool isMovingX = false, isMovingY = false;
+    private Vector2 currentPos, targetPos, fieldY, moveXY, startPos;
     private Vector4 currentFied;
+    private float speed = 250f;
 
     void Start()
     {
@@ -19,14 +19,34 @@ public class PlayerMoving : MonoBehaviour
         five_field = GameObject.FindGameObjectWithTag("FiveField");
         currentPos = transform.position;
         fieldY = new Vector2(Screen.height/2+Screen.width/2, Screen.height/2-Screen.width/2);
-        // Debug.Log(fieldY);
+        startPos = transform.position;
     }
 
     [System.Obsolete]
     void Update()
     {
-        
         currentPos = transform.position;
+        // Debug.Log(transform.position.y);s
+
+        // if(isMovingX && transform.position.x != moveXY.x)
+        // {
+        //     transform.position = Vector2.MoveTowards(transform.position, new Vector2(moveXY.x, transform.position.y), speed * Time.deltaTime);
+        // } 
+        // else
+        // {
+        //     isMovingX = false;
+        // }
+
+        // if(isMovingY && transform.position.y != moveXY.y && !isMovingX)
+        // {
+        //     Debug.Log("LLLLLLLLLLLL");
+        //     transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, moveXY.y), speed * Time.deltaTime);
+        // } 
+        // else
+        // {
+        //     isMovingY = false;
+        // }
+        
         if(five_field.active)
         {
             level = 5;
@@ -40,18 +60,45 @@ public class PlayerMoving : MonoBehaviour
             level = 9;
         }
 
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) && !isMovingX && !isMovingY) {
             targetPos = Input.mousePosition;
             float cellBoard = Screen.width/(2*level);
             currentFied = new Vector4(currentPos.x - cellBoard, currentPos.x + cellBoard, currentPos.y + cellBoard, currentPos.y - cellBoard); // cell borders
             if(targetPos.y < fieldY.x && targetPos.y > fieldY.y && (targetPos.x > currentFied.y || targetPos.x < currentFied.x) && (targetPos.y > currentFied.z || targetPos.y < currentFied.w))
-                Debug.Log("Norm");
-            else
             {
-                Debug.Log("Bad");
+                // Debug.Log(level - Math.Truncate((targetPos.y-Screen.width/2)/(cellBoard*2)));
+                // Debug.Log(transform.position.x);
+                moveXY.x = (float)(cellBoard*2*Math.Truncate(targetPos.x/(cellBoard*2)))+startPos.x;
+                moveXY.y = (float)(cellBoard*2*Math.Truncate((targetPos.y-Screen.width/2)/(cellBoard*2)))+(Screen.height - startPos.y);
+                // isMovingX = true;
+                // isMovingY = true;
+                StartCoroutine(MoveCoroutineX(moveXY.x));
             }
-            // Debug.Log(Input.mousePosition);
-            // Debug.Log(Screen.width +"s"+ Screen.height);
         }
+    }
+    
+    IEnumerator MoveCoroutineX(float moveTo)
+    {
+        isMovingX = true;
+        while (transform.position.x != moveTo)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(moveXY.x, transform.position.y), speed * Time.deltaTime);
+            yield return null;
+        }
+
+        isMovingX = false;
+        isMovingY = true;
+        StartCoroutine(MoveCoroutineY(moveXY.y));
+
+    }
+    
+    IEnumerator MoveCoroutineY(float moveTo)
+    {
+        while (transform.position.y != moveTo)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, moveXY.y), speed * Time.deltaTime);
+            yield return null;
+        }
+        isMovingY = false;
     }
 }
